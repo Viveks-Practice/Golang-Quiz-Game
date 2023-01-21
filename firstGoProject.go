c36412a6c987997a8894a42edc5c7281bf802d69
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-01-20 16:38:53 -0800

    first commit, asking user for their name

---
+++ firstGoProject.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to my quiz game!")
+
+	fmt.Printf("Enter your name: ")
+
+	var name string
+
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello, %v, welcome to the game!", name)
+
+}
