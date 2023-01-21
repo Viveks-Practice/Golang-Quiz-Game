53bba58ef09f54799b6794cba893e3ef5e38fb29
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-01-20 17:10:06 -0800

    covering reading and outputting to the command line in golang

--- firstGoProject.go
+++ firstGoProject.go
@@ -11,6 +11,22 @@ func main() {
 
 	fmt.Scan(&name)
 
-	fmt.Printf("Hello, %v, welcome to the game!", name)
+	fmt.Printf("Hello, %v, welcome to the game!\n", name)
 
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+
+	if age >= 10 {
+		fmt.Println("Yay you can play!")
+	} else {
+		fmt.Println("You cannot play!")
+		return
+	}
+
+	fmt.Printf("What is the densest object in the universe? ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer, &answer2)
+	fmt.Println(answer, answer2)
 }
