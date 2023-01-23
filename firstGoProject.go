35563aceefffdaa8527db21dbfcd27a25eabf0fc
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-01-23 14:21:51 -0800

    finished the golang basics project

--- firstGoProject.go
+++ firstGoProject.go
@@ -24,9 +24,33 @@ func main() {
 		return
 	}
 
+	score := 0
+	num_questions := 2
+
 	fmt.Printf("What is the densest object in the universe? ")
 	var answer string
 	var answer2 string
 	fmt.Scan(&answer, &answer2)
-	fmt.Println(answer, answer2)
+
+	if answer+" "+answer2 == "Black Hole" || answer+" "+answer2 == "black hole" {
+		fmt.Println("Correct!")
+		score += 1
+	} else {
+		fmt.Println("Incorrect!")
+	}
+
+	fmt.Printf("How many planets are there in our solar system? ")
+	var planets int
+	fmt.Scan(&planets)
+
+	if planets == 8 {
+		fmt.Println("Correct!")
+		score += 1
+	} else {
+		fmt.Println("Incorrect!")
+	}
+
+	fmt.Printf("You scored %v out of %v. ", score, num_questions)
+	percent := (float64(score) / float64(num_questions)) * 100
+	fmt.Printf("You scored: %v%%", percent)
 }
